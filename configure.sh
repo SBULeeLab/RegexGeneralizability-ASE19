5885b6e5ab871144151a5d7b066a17ee8e6edf76
Jamie Davis <davisjam@vt.edu>
2019-09-24 15:38:49 -0400

    configuration scripts

--- configure.sh
+++ configure.sh
@@ -5,38 +5,97 @@ echo "Configuring repo. I hope you use Ubuntu..."
 set -x
 set -e
 
-# Need this for the vuln-regex-detector installation
-echo "Installing nvm"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-# Source so nvm is in path now
-touch ~/.bashrc && . ~/.bashrc
+#####
+# corpus/
 
-echo "Installing and using node v8.9.3. The code works at this level, so insulate against future breaking changes."
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install v8.9.3
-nvm alias default v8.9.3
+echo "Configuring corpus creation tools"
+pushd corpus/corpus-creation/
+  echo "  Tools for extraction via static analysis"
+  pushd static-analysis/
+    pushd java/regex-extractor
+    ./build.pl
+    popd
 
-## Dependencies
+    pushd js
+    ./build.pl
+    popd
 
-echo "Configuring submodule: vuln-regex-detector"
-pushd analysis/performance/vuln-regex-detector
-./configure
+    pushd ts
+    ./build.pl
+    popd
+
+    pushd python
+    ./build.pl
+    popd
+	popd
+
+  echo "  Tools for extraction via program instrumentation"
+  pushd program-instrumentation
+		pushd python
+		./build.pl
+		popd
+	popd
+
+#####
+# measurement-instruments/
+
+echo "Configuring regex measurement tools"
+
+echo "Installing nvm (for vuln-regex-detector)"
+installNVM()
+
+echo "Installing wine (so we can run AutomataCLI.exe)"
+installWine()
+
+pushd measurement-instruments/
+./build.pl
+
+  echo "  Configuring worst-case performance detection"
+  pushd worst-case-performance/
+  ./build.pl
+  popd
 popd
-
-echo "Configuring per-language regex extractors"
-pushd data/production-regexes/static-extractors/
-./compile-extractors.pl
-popd
-
-echo "Configuring per-language regex drivers"
-pushd analysis/test-regex-behavior-in-language/
-./compile-testers.pl
-popd
-
-echo "Compiling input generators"
-pushd analysis/semantic/input-generation/generators/
-./compile-input-generators.pl
-popd 
 
 echo "Configuration complete. I hope everything works!"
+
+############
+# Functions
+############
+
+installWine() {
+  if command_exists wine ; then
+    echo "wine already installed"
+    return
+  fi
+  echo "Installing wine -- sudo required"
+  sudo dpkg --add-architecture i386
+  wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+  sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
+  sudo apt-get update
+  sudo apt-get install --install-recommends winehq-stable
+
+  if command_exists wine ; then
+    echo "wine successfully installed"
+    return
+  fi
+  echo "Could not install wine"
+  exit 1
+}
+
+installNVM() {
+  if command_exists nvm ; then
+    echo "nvm already installed"
+    return
+  fi
+  echo "Installing nvm"
+
+  # TODO My notes say we need this for the vuln-regex-detector installation. Do we?
+  echo "Installing nvm"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+  # Source so nvm is in path now
+  touch ~/.bashrc && . ~/.bashrc
+}
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
