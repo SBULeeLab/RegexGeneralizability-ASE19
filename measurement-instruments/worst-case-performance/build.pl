6f6c96348f16356eb448e62b27fc3297a7843ae3
Jamie Davis <davisjam@vt.edu>
2019-09-24 16:12:01 -0400

    bundle VRD deps

--- measurement-instruments/worst-case-performance/build.pl
+++ measurement-instruments/worst-case-performance/build.pl
@@ -17,6 +17,7 @@ chkcmd("tar -xzvf $tarball");
 chkcmd("mv $artifactName $dest");
 
 print "Configuring the artifact\n  (This may prompt for sudo to install dependencies)\n";
+fixDetectors($dest);
 chkcmd("cd $dest; ./configure");
 
 sub chkcmd {
@@ -32,4 +33,29 @@ sub runcmd {
   print "$cmd\n";
   print `$cmd`;
   return $? >> 8;
+}
+
+sub fixDetectors {
+  my ($vrdRoot) = @_;
+  my $detectorSrc = "$vrdRoot/src/detect/src/detectors/";
+
+  my $detector2paths = {
+    "weideman" => {
+      "dest": "weideman-RegexStaticAnalysis",
+      "artifact": "weideman-ASE19-artifact.tar.gz",
+      "untarDir": "RegexStaticAnalysis-ASE19-artifact",
+    },
+    "shen" => {
+      "path": "shen-ReScue",
+      "artifact": "shen-ASE19-artifact.tar.gz",
+      "untarDir": "ReScue-ASE19-artifact",
+    }
+  }
+
+  for my $detector (keys %$detector2paths) {
+    print "Fixing up $detector\n";
+    chkcmd("rm -rf $detectorSrc/$detector2paths->{$detector}->{dest}");
+    chkcmd("cp $detector2paths->{$detector}->{artifact} $detectorSrc");
+    chkcmd("cd $detectorSrc; tar -xzvf $detector2paths->{$detector}->{artifact}; mv $detector2paths->{$detector}->{untarDir} $detector2paths->{$detector}->{dest}");
+  }
 }
